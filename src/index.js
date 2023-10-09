62f84553b53345f2332f664c46062b8b5186fbf2
Promethiyas <valentin.bravo@ynov.com>
2023-10-09 18:07:43 +0200

    modification code et tentative pour environnement et content type (à l'air de fonctionner)

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var pingport = process.env.PING_LISTEN_PORT;
 var app = express();
 app.use(function (req, res, next) {
     if (req.originalUrl != "/ping") {
         res.send('ERROR 404');
     }
     else {
+        /*res.writeHead(200, {
+            'Content-Type' : 'application/json'
+        });
+        let headers = req.headers;
+        let res1 = JSON.stringify(headers)
+        res.end(headers)*/
         var headers = req.headers;
         res.json(headers);
     }
 });
-app.get('/', function (req, res) {
+/*app.get('/', (req, res) => {
     res.send('ERROR 404');
-});
+})
 app.get('/ping', function (req, res) {
-    var headers = req.headers;
-    res.json(headers);
-});
-app.listen(3000, function () {
+    let headers = req.headers;
+    res.json(headers)
+});*/
+app.listen(pingport, function () {
     console.log('The application is listening on port 3000!');
 });
